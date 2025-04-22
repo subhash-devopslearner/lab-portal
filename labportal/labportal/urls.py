0df1b20ebd6edbd7fd343ad412ba2345871bbbd4
Subhash Bhalerao <subhash.devops.learner@gmail.com>
2025-04-22 17:11:08 +0530

    Add Labexam app in settings nad home navigation

--- labportal/labportal/urls.py
+++ labportal/labportal/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('experiments/', include('experiments.urls')),    
+    path('experiments/', include('experiments.urls')),
+    path('labexam/', include('labexam.urls')),    
 ]
 
 if settings.DEBUG:
